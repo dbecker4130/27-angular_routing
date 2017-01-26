cb24bdc5bae8d118fe42262367789b4f43ef0cf3
Daniel Becker <dbecker4130@gmail.com>
2017-01-26 11:38:47 -0800

    debugging with Gio

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,32 @@
+'use strict';
+
 const webpackConfig = require('./webpack.config.js');
 webpackConfig.entry = {};
 
 module.exports = function(config) {
   config.set({
-
+    webpack: webpackConfig,
     basePath: '',
-
     frameworks: ['jasmine'],
-
     files: [
-      '"test/**/*-test.js"'
-    ],
+      'test/**/*-test.js',
+      'app/entry.js',
+      'node_modules/angular-mocks/angular-mocks.js'
 
+    ],
     exclude: [
     ],
-
     preprocessors: {
-      'test/**/*-test.js': ['webpack']
+      'test/**/*-test.js': ['webpack'],
+      'app/entry.js': ['webpack'],
     },
-
-    reporters: ['progress'],
-
+    reporters: ['mocha'],
     port: 9876,
-
     colors: true,
-
     logLevel: config.LOG_INFO,
-
     autoWatch: true,
-
-    browsers: ['PhantomJS'],
-
+    browsers: ['Chrome'],
     singleRun: false,
-
     concurrency: Infinity
   });
 };
